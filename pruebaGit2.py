d17af07be19b9651046288746bd3e406ea04e8cb
Andres Quintero <quinte05@gmail.com>
2023-08-15 18:55:00 -0500

    probando cambio en ramab en solo el archivo pruebaGit2

--- pruebaGit2.py
+++ pruebaGit2.py
@@ -2,3 +2,6 @@ print("recontra aprendiendo GIT")
 print("experto en GIT")
 print("super experto en GIT")
 print("chanchito q sabe GIT")
+for i in range(5):
+    print (f"voy en la vuelta {i} y estoy probando unir una rama a la master y luego subirlo al rep rem")
+    
